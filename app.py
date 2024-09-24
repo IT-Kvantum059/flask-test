38b680777cb1e66f7461fc538d3f09928232b2bf
Test <alarionov93@mail.ru>
2024-09-24 18:17:33 +0500

    added render temlate

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/", methods=['GET'])
 def hello_world():
     a = 123
-    return f'<p>Hello, world!{a}</p>'
+    return render_template('base.html')
 
 @app.route("/login", methods=['POST'])
 def login():
